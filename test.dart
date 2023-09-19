a497e4b23ae7db92048127ab2f3e2706f4ccb46d
unknown <rojotiana.tsiverisoa@gmail.com>
2023-09-19 13:51:59 +0300

    ajout d'une fonction de calcul d'une puissance

--- test.dart
+++ test.dart
@@ -6,8 +6,22 @@ int factoriel(int n) {
   return result;
 }
 
+int puissance(int n, int exposant) {
+  int result = 1;
+  for (int i = 1; i <= exposant; i++) {
+    result *= n;
+  }
+  return result;
+}
+
+
 void main() {
   // calcul d'un factoriel d'un nombre
   int nFactoriel = 4;
   print("${nFactoriel}! = ${factoriel(nFactoriel)}");
+
+  // calcul de puissance
+  int n1 = 3;
+  int exposant = 2;
+  print("${n1}^${exposant} = ${puissance(n1, exposant)}");
 }
